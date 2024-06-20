bd0e8fd9bd40bc90dd80d537b4c465e1a5f0f121
Mateus Marchinack <167545286+Marchinack@users.noreply.github.com>
2024-06-20 13:00:53 -0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,13 +12,13 @@ public class Main {
         if (imc < 18.5) {
             System.out.println(" Abaixo do peso");
         } 
-        if (imc >= 18.5 && imc < 25) {
+        if (imc > 18.5 && imc < 25) {
             System.out.println(" Peso normal");
         } 
-        if (imc >= 25 && imc < 30) {
+        if (imc > 25 && imc < 30) {
             System.out.println(" Sobrepeso");
         } 
-        if (imc >= 30) {
+        if (imc > 30) {
             System.out.println(" Obesidade");
         } 
     }
